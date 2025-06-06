31f11f5116555523d91d68b453ebeebf0dbabc06
AmanChaurasiya5 <amanchaurasiya12102002@gmail.com>
2025-06-06 16:59:39 +0530

    contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -14,7 +14,7 @@ const contact = () => {
       <div className="flex flex-col justify-center items-start gap-6">
         <p className='font-semibold text-xl text-gray-600'>Our Store</p>
         <p className='text-gray-500 '>Mumbai ,Maharashtra<br /> INDIA</p>
-        <p className='text-gray-500 '> +91 9820392165 <br />Email: <a href="mailto:testdevaman@gmail.com"></a>  </p>
+        <p className='text-gray-500 '> +91 9820392165 <br />Email: <a href="mailto:testdevaman@gmail.com">testdevaman@gmail.com</a>  </p>
         <p className='font-semibold text-xl text-gray-600'>Careers at forever</p>
                 <p className='text-gray-500 '>Learn more about our teams and job openings.</p>
       <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
