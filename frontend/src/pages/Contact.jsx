e60fa51a5f9e106d81dfcbcd0a4eb23d55604450
AmanChaurasiya5 <amanchaurasiya12102002@gmail.com>
2025-06-06 16:58:11 +0530

    contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -13,8 +13,8 @@ const contact = () => {
       <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt=""/>
       <div className="flex flex-col justify-center items-start gap-6">
         <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-        <p className='text-gray-500 '>%4709 Wills Station <br />Suite 350, Washington, USA</p>
-        <p className='text-gray-500 '>Tel :(415) 555-0132 <br />Email: admin@forever.com</p>
+        <p className='text-gray-500 '>Mumbai ,Maharashtra<br /> INDIA</p>
+        <p className='text-gray-500 '> +91 9820392165 <br />Email: <a href="mailto:testdevaman@gmail.com"></a>  </p>
         <p className='font-semibold text-xl text-gray-600'>Careers at forever</p>
                 <p className='text-gray-500 '>Learn more about our teams and job openings.</p>
       <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
